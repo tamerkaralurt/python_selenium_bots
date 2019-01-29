12cdfad284afbd6d8213515f67f69cbe63603b88
Tamer <tamerkaralurt@gmail.com>
2019-01-30 01:46:11 +0300

    corum botu tamamlandı

--- selenium_corum.py
+++ selenium_corum.py
@@ -14,16 +14,48 @@ sheet1.write(0, 3, "Firma Faks")
 sheet1.write(0, 4, "Firma Web")
 
 
+def getSqlTxt(id, firma_adi, firma_email, firma_tel, firma_faks, firma_web):
+    try:
+        with open("corum.txt", "a+") as errorfile:
+            errorfile.write("INSERT INTO tapbis.bot_firmas (firma_id,firma_adi,firma_email,firma_telefon,firma_fax,firma_web_adres,created_at,updated_at) VALUES ('" + id + "','" + firma_adi + "','" + firma_email + "','" + firma_tel + "','" + firma_faks + "','" + firma_web + "','2019-01-30 01:33:26','2019-01-30 01:33:26');")
+            errorfile.write("\n")
+            errorfile.close()
+    except Exception as e:
+        print("Hata: getInfo: " + str(e))
+        with open("errors.txt", "a+") as errorfile:
+            errorfile.write("Corum -- ")
+            errorfile.write("Hata: getInfo: " + str(e))
+            errorfile.write("\n")
+            errorfile.close()
+
+
 def getInfo(row):
     global record_num
     try:
         # Firma bilgilerini alma islemi yapilacak.
         # /html/body/div/div[1]/form/div[7]/div/div/div/div/div[2]/div[2]/div/h2/a
-        firma_adi = driver.find_element_by_xpath("/html/body/div/div[1]/form/div[7]/div/div/div/div/div["+str(row)+"]/div[2]/div/h2/a").text
-        firma_email = driver.find_element_by_xpath("/html/body/div/div[1]/form/div[7]/div/div/div/div/div["+str(row)+"]/div[2]/div/div[1]/ul/li[4]").text
-        firma_tel = driver.find_element_by_xpath("/html/body/div/div[1]/form/div[7]/div/div/div/div/div["+str(row)+"]/div[2]/div/div[1]/ul/li[2]").text
-        firma_faks = driver.find_element_by_xpath("/html/body/div/div[1]/form/div[7]/div/div/div/div/div["+str(row)+"]/div[2]/div/div[1]/ul/li[3]").text
-        firma_web = driver.find_element_by_xpath("/html/body/div/div[1]/form/div[7]/div/div/div/div/div["+str(row)+"]/div[2]/div/div[1]/ul/li[1]").text
+        firma_adi = driver.find_element_by_xpath(
+            "/html/body/div/div[1]/form/div[7]/div/div/div/div/div[" + str(row) + "]/div[2]/div/h2/a").text
+        firma_email = driver.find_element_by_xpath(
+            "/html/body/div/div[1]/form/div[7]/div/div/div/div/div[" + str(row) + "]/div[2]/div/div[1]/ul/li[4]").text
+        firma_tel = driver.find_element_by_xpath(
+            "/html/body/div/div[1]/form/div[7]/div/div/div/div/div[" + str(row) + "]/div[2]/div/div[1]/ul/li[2]").text
+        firma_faks = driver.find_element_by_xpath(
+            "/html/body/div/div[1]/form/div[7]/div/div/div/div/div[" + str(row) + "]/div[2]/div/div[1]/ul/li[3]").text
+        firma_web = driver.find_element_by_xpath(
+            "/html/body/div/div[1]/form/div[7]/div/div/div/div/div[" + str(row) + "]/div[2]/div/div[1]/ul/li[1]").text
+
+        if firma_web == "Web Adresi Yok":
+            firma_web = ""
+
+        if firma_faks == "Faks Bilgisi Yok":
+            firma_faks = ""
+
+        if firma_tel == "Telefon Bilgisi Yok":
+            firma_tel = ""
+
+        if firma_email == "E-Posta Bilgisi Yok":
+            firma_email = ""
 
         # excel kayit alani
         sheet1.write(record_num, 0, firma_adi)
@@ -32,6 +64,7 @@ def getInfo(row):
         sheet1.write(record_num, 3, firma_faks)
         sheet1.write(record_num, 4, firma_web)
         book.save("Corum.xls")
+        getSqlTxt(str(14), firma_adi, firma_email, str(firma_tel), str(firma_faks), firma_web)
         record_num += 1
         print("Kayit Edildi: " + firma_adi + " / " + firma_email + " / " + firma_tel[0:5] + " / " + firma_faks[0:5] + " / " + firma_web[0:5])
         timeSleep(2, "getInfo")
@@ -49,7 +82,8 @@ def getCompanies():
     # /html/body/div/div[1]/form/div[7]/div/div/div/div/div[2]
     items = driver.find_elements_by_class_name('blog-item')
     for item in items:
-        getInfo(items.index(item)+1)
+        getInfo(items.index(item) + 1)
+
 
 def getMainPage():
     try:
